b66e846ac14ca8d27b7221027cb40fbd8eb773e9
Javier Moncada <javierandresmd@ufps.edu.co>
2021-05-23 13:28:31 -0500

    Diseño del Home terminado

---
+++ app/src/main/java/co/edu/ufps/criaderoapp/EstanqueActivity.kt
@@ -0,0 +1,15 @@
+package co.edu.ufps.criaderoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class EstanqueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estanque)
+
+
+    }
+}
--- app/src/main/java/co/edu/ufps/criaderoapp/EstanqueFragment.kt
+++ app/src/main/java/co/edu/ufps/criaderoapp/EstanqueFragment.kt
@@ -34,7 +34,7 @@ class EstanteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_estante, container, false)
+        return inflater.inflate(R.layout.fragment_estanque, container, false)
     }
 
     companion object {
