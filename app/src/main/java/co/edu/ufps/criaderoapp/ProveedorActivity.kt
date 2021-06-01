37a2032c045ffaa130fe2c3acccd042f43a83c72
Javier Moncada <javierandresmd@ufps.edu.co>
2021-05-31 19:40:51 -0500

    Creando el activity para proveedores

---
+++ app/src/main/java/co/edu/ufps/criaderoapp/ProveedorActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.ufps.criaderoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProveedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_proveedor)
+    }
+}
---
+++ app/src/main/java/co/edu/ufps/criaderoapp/ProveedorEntity.kt
@@ -0,0 +1,10 @@
+package co.edu.ufps.criaderoapp
+
+data class ProveedorEntity(
+        val id :String,
+        val codigo: String,
+        val nombre : String,
+        val direccion : String,
+        val responsable : String,
+        val contacto : String
+)
