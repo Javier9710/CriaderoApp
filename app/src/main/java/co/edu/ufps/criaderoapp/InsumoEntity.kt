8a33dff6e834315ae04ef7f15b24d17a80d5d6df
Javier Moncada <javierandresmd@ufps.edu.co>
2021-06-06 23:47:35 -0500

    Cargando datos desde la base de datos en nube (FireBase)

--- app/src/main/java/co/edu/ufps/criaderoapp/InsumoEntity.kt
+++ app/src/main/java/co/edu/ufps/criaderoapp/InsumoEntity.kt
@@ -1,10 +1,10 @@
 package co.edu.ufps.criaderoapp
 
 data class InsumoEntity(
-        val id :String,
-        val nombre: String,
-        val cantidad : String,
-        val proveedor : String
+        val id :String="",
+        val nombre: String="",
+        val cantidad : String="",
+        val proveedor : String=""
 
 )
 
--- app/src/main/java/co/edu/ufps/criaderoapp/InsumosFragment.kt
+++ app/src/main/java/co/edu/ufps/criaderoapp/InsumosFragment.kt
@@ -1,12 +1,16 @@
 package co.edu.ufps.criaderoapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -43,7 +47,7 @@ class InsumosFragment : Fragment() {
         var linearLayout = LinearLayoutManager(context)
         linearLayout.orientation = LinearLayoutManager.VERTICAL
         contenedorInsumos.layoutManager = linearLayout
-        adapterInsumo = AdapterInsumo(context, cargarDatos(), R.id.insumo)
+        adapterInsumo = AdapterInsumo(context, cargarDatosFireBase(), R.id.insumo)
         contenedorInsumos.adapter = adapterInsumo
         return view
     }
@@ -60,23 +64,57 @@ class InsumosFragment : Fragment() {
         return insumos;
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InsumosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InsumosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    fun cargarDatosFireBase(): ArrayList<InsumoEntity> {
+
+        val insumos = ArrayList<InsumoEntity>()
+        val dataBase : FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef : DatabaseReference = dataBase.getReference()
+        myRef.child("insumos").addValueEventListener(object : ValueEventListener
+        {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    insumos.clear()
+                    for (data in snapshot.children) {
+                        val insumo = data.getValue(InsumoEntity::class.java)
+                        insumos.add(insumo as InsumoEntity)
+                        adapterInsumo.notifyDataSetChanged()
+                    }
+
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+
+                Log.w("InsumosFragment", "Carga cancelada", error.toException())
+            }
+
+        })
+
+        return insumos
     }
+
+
+
+
+            companion object {
+            /**
+             * Use this factory method to create a new instance of
+             * this fragment using the provided parameters.
+             *
+             * @param param1 Parameter 1.
+             * @param param2 Parameter 2.
+             * @return A new instance of fragment InsumosFragment.
+             */
+            // TODO: Rename and change types and number of parameters
+            @JvmStatic
+            fun newInstance(param1: String, param2: String) =
+                    InsumosFragment().apply {
+                        arguments = Bundle().apply {
+                            putString(ARG_PARAM1, param1)
+                            putString(ARG_PARAM2, param2)
+                        }
+                    }
+        }
+
 }
